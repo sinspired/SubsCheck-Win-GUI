b1eaac4c5e4cedbba50fdfe5d191031a379f5040
sinspired <ggmomo@gmail.com>
2025-11-20 03:58:24 +0800

    chore: bump version to v2.6.0.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.5.0.2")]
-[assembly: AssemblyFileVersion("2.5.0.2")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
